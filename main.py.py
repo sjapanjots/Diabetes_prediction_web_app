d891bc8ce997ac554b757d3827a6ce92e6e02d9a
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2023-09-01 18:34:02 +0530

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -18,7 +18,7 @@ loaded_model = pickle.load(open( 'trained_model.sav' , 'rb'))
 def diabetes_prediction(input_data):
     
     # changing the input data to numpy array 
-    input_data_as_numpy_array = np.asaaray(input_data)
+    input_data_as_numpy_array = np.asarray(input_data)
     
     # reshape the arrasy as we are predicting for one instance 
     input_data_reshaped = input_data_as_numpy_array.reshape(-1,1)
