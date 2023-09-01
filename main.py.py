588a3bec144c08fcf5dce0519b17dadacf191c5c
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2023-09-01 13:46:05 +0530

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -66,12 +66,3 @@ if __name__ == '__main__':
     main()
 
 
-
- st.markdown("""
-    ---
-    Model is not 100% Accurate and Build Only with Given Data
-
-    
-    Design and Developed by Japanjot Singh  (https://github.com/japanjot)
-    """)
-    
