b91fc998e11f9075ed9bc99a062ebd827d2433d7
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2023-11-26 17:56:28 +0530

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -80,7 +80,7 @@ hide_st_style = """
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
-              Design and Developed by Japanjot Singh 
+              Design and Developed by Ishan Sethi 
             """
 st.markdown(hide_st_style , unsafe_allow_html=True)
 
